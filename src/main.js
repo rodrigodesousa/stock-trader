3a59f0edbcd12bd00799ad5bdde92b40de345c33
rodrigodesousa <rodrigudesousa@gmail.com>
2021-03-05 18:15:25 -0400

    get and put data with vue-resource and firebase

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes } from '@/routes/index'
 import store from '@/store/index'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vuejs-stock-trader-169c1.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString()
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuex  from 'vuex'
 import stocks from '@/store/modules/stocks'
 import portfolio from '@/store/modules/portfolio'
+import * as actions from '@/store/actions'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    actions,
     modules: {
         stocks,
         portfolio
