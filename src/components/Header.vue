cb04695105d51564a9711a3e5c174bda5b5e0392
rodrigodesousa <rodrigudesousa@gmail.com>
2021-03-04 20:48:29 -0400

    component stock

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,29 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="navbar-header">  
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
       <router-link to="/" class="navbar-brand">Stock Trader</router-link>
     </div>
-    <div class="collapse navbar-collapse">
-      <ul class="navbar-nav mr-auto">
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
         <router-link to="/portfolio" active-class="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
         <router-link to="/stocks" active-class="active" tag="li"><a class="nav-link">Stocks</a></router-link>
       </ul>
-      <ul class="navbar-nav">
-        <li><a href="#" class="nav-link">End Day</a></li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Save & Load
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Save Data</a>
-            <a class="dropdown-item" href="#">Load Data</a>
-          </div>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">End Day</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Save Data</a></li>
+            <li><a href="#">Load Data</a></li>
+          </ul>
         </li>
       </ul>
-    </div>
-  </nav>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 </template>
 <script>
 export default {
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,8 +1,22 @@
 <template>
-    <h1>The Stock - Stocks component</h1>
+    <div>
+        <Stock v-for="stock in stocks" :key="stock.id" />
+    </div>
 </template>
 <script>
+import Stock from '@/components/stocks/Stock'
+
 export default {
-    data: () => ({})
+    components: {
+        Stock
+    },
+    data: () => ({
+        stocks: [
+            { id: 1, name: 'BMW', price: 110 },
+            { id: 2, name: 'Google', price: 210 },
+            { id: 3, name: 'Facebook', price: 180 },
+            { id: 4, name: 'Apple', price: 200 },
+        ]
+    })
 }
 </script>
